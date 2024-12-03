af09a36eec3846c506832bbf294f48293144564f
SqushiLoL <mariusgreiersen@gmail.com>
2024-12-03 17:27:11 +0100

    disassemble.c + simulate.c (empty)

---
+++ a5/disassemble.c
@@ -0,0 +1,7 @@
+#include <disassemble.h>
+#include <memory.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+disassemble() {}
---
+++ a5/simulate.c
@@ -0,0 +1,8 @@
+#include <simulate.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+simulate() {}
