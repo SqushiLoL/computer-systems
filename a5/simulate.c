99da73670ebd67caedf842b90054501184c8c56c
KBPalle <ottopalving@gmail.com>
2024-12-03 22:21:13 +0100

    shut up simulate

--- a5/simulate.c
+++ a5/simulate.c
@@ -1,9 +1,10 @@
 #include "simulate.h"
 
-#include <stddef.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-simulate() {}
+struct Stat simulate(struct memory* mem, int start_addr, FILE* log_file,
+                     struct symbols* symbols) {
+    //dummy Stat object
+    struct Stat dummy_stat = {0};
+    
+    //do nothing
+    return dummy_stat;
+}
